403b03299cc58b244c69ee5fa2166885508fd066
Lorenzo M. Alù <lorenzo.alu@gmail.com>
2021-05-06 16:38:14 +0200

    resolve bugs

--- database/migrations/2021_03_18_135213_create_categories_table.php
+++ database/migrations/2021_03_18_135213_create_categories_table.php
@@ -4,8 +4,8 @@
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateCategoriesTable extends Migration
 {
