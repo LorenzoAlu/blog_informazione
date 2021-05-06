6111a2f483384f81dcb26b19cc60c0cd9e16b02e
Lorenzo M. Alù <lorenzo.alu@gmail.com>
2021-05-07 01:07:10 +0200

    fixed bugs

--- database/migrations/2021_03_18_135213_create_categories_table.php
+++ database/migrations/2021_03_18_135213_create_categories_table.php
@@ -2,7 +2,7 @@
 
 
 
-use app\Models\Category;
+use App\Models\Category;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
